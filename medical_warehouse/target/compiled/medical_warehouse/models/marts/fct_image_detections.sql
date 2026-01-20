98cf56bbd83a28bc1dc2b0e555dd6983d4d9a6ca
Legend373 <y918473@gmail.com>
2026-01-20 11:49:46 +0300

    job:performing yolo object detection on the images

---
+++ medical_warehouse/target/compiled/medical_warehouse/models/marts/fct_image_detections.sql
@@ -0,0 +1,17 @@
+WITH detections AS (
+    SELECT
+        ed.message_id,
+        tm.channel_name,
+        d.date_key,
+        ed.detected_class,
+        ed.confidence_score,
+        ed.image_category
+    FROM enrichment.image_detections ed
+    JOIN raw.telegram_messages tm
+      ON ed.message_id = tm.message_id
+    JOIN analytics.dim_dates d
+      ON DATE(tm.message_date) = d.full_date
+)
+
+SELECT *
+FROM detections
